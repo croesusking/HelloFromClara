f2d6e9b7696e9523c64c27c2b653d5686b70e9fd
audreymae <mon19009@byui.edu>
2021-02-06 12:25:48 -0800

    Editing file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello,my name is Clara.");
+
+        System.out.println("Hello Clara Chan, my name is Audrey.");
     }
 }
